5f38057914ae1012490b52278f89c173e2638480
Kris-HV <71531490+Kris-HV@users.noreply.github.com>
2023-12-11 16:59:59 -0600

    Adding Files and Initializing Project
    Blahblah

---
+++ Assets/Scripts/Level_Handler.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NLevel_Handler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Level_Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
