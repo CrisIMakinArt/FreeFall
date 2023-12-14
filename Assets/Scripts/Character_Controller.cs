cc98f22e780be57ceea87ad649da2e3bf9198dd8
Cris Cee <donovan.dh84@gmail.com>
2023-12-14 17:45:42 -0600

    PlayerMovement update

--- Assets/Scripts/Character_Controller.cs
+++ Assets/Scripts/Character_Controller.cs
@@ -4,13 +4,42 @@ using System.Security.Cryptography;
 using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
+    public float playerVelocity = 0.1f;
+    private float lastY;
+    public float FallingThreshold = -0.001f; 
+    [HideInInspector]
+    public bool Falling = false;
+    public float Acceleration = 0.2f;
     public float MovementSpeed = 6;
     public float JumpForce = 1;
     private Rigidbody2D _rigidbody;
     private void Start()
     {
-
+        lastY = transform.position.y;
         _rigidbody = GetComponent<Rigidbody2D>();
+        InvokeRepeating("acceleratePlayer", 0, 1.0f);
+    }
+    void acceleratePlayer()
+    {
+        float distancePerSecondSinceLastFrame = (transform.position.y - lastY) * Time.deltaTime;
+        lastY = transform.position.y; 
+        if (distancePerSecondSinceLastFrame < FallingThreshold)
+        {
+            Falling = true;
+        }
+        else
+        {
+            Falling = false;
+        }
+
+        if (Falling)
+        {
+        playerVelocity += Acceleration;
+        }
+        else 
+        {
+            playerVelocity = Acceleration;
+        }
     }
 
     private void FixedUpdate()
@@ -21,6 +50,8 @@ public class PlayerMovement : MonoBehaviour
         {
             _rigidbody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
         }
+        _rigidbody.gravityScale = playerVelocity;
+
     }
 
 
@@ -28,4 +59,5 @@ public class PlayerMovement : MonoBehaviour
     {
         transform.position = new Vector3(0, 0, 0);
     }
+    
 }
